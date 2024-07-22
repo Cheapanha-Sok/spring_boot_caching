4a62471517e34620dab0a9eb6681c39fc7244d9e
sokcheapanha <sokcheapanha11@gmail.com>
2024-07-22 10:18:25 +0700

    update configuration

--- src/main/kotlin/com/example/cache/config/RedisConfiguration.kt
+++ src/main/kotlin/com/example/cache/config/RedisConfiguration.kt
@@ -1,6 +1,5 @@
 package com.example.cache.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
@@ -8,8 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.RedisSerializer
@@ -19,19 +17,6 @@ import java.time.Duration
 @Configuration
 @EnableCaching
 class RedisConfiguration {
-    @Value("\${spring.data.redis.host}")
-    val host: String? = null
-
-    @Value("\${spring.data.redis.port}")
-    val port: Int? = null
-
-    @Value("\${spring.cache.redis.time-to-live}")
-    val timeToLive: Long? = null
-
-    @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(RedisStandaloneConfiguration(host!!, port!!))
-    }
 
     @Bean
     fun redisSerializer(): GenericJackson2JsonRedisSerializer {
@@ -41,7 +26,7 @@ class RedisConfiguration {
     @Bean
     fun cacheManager(connectionFactory: RedisConnectionFactory, redisSerializer: RedisSerializer<Any>): CacheManager {
         val default: RedisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-            .entryTtl(Duration.ofMinutes(timeToLive!!))
+            .entryTtl(Duration.ofMinutes(5))
             .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
 
         return RedisCacheManager.builder(connectionFactory)
